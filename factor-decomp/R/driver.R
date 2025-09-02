55000e0d3de74755c8411b3452c7138f79c37ba6
Derek Peterson <derek.peterson@gpmtreit.com>
2025-09-02 16:47:32 -0500

    further build of driver.R

--- factor-decomp/R/driver.R
+++ factor-decomp/R/driver.R
@@ -4,6 +4,18 @@ source("R/paths.R")         # defines data_raw_dir(), data_clean_dir()
 source("R/load_factors.R")  # defines load_ff_factors_daily()
 source("R/returns_utils.R") # defines prices_to_returns(), align_with_factors()
 
-asset_rets <- prices_to_returns()          # reads data_raw/ticker_prices.csv by default
-aligned    <- align_with_factors(asset_rets)  # factors auto-loaded
+tickers <- c("SPY", # S&P 500, broad U.S. market
+             "IWM", # Russell 2000 small caps
+             "EFA", # Developed Markets ex-US
+             "EEM", # Emerging Markets
+             "LQD", # IG corporate bonds
+             "HYG", # HY corporate bonds
+             "TLT", # 20+ yr treasuries
+             "DBC", # Commodity index
+             "VNQ") # REITs
+
+asset_prices <- download_prices(tickers)   
+asset_returns <- prices_to_returns()
+
+aligned    <- align_with_factors(asset_returns)  # factors auto-loaded
 dplyr::glimpse(aligned)
