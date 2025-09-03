2411c88299ac269fb42e18d3795a86a1dcb9acf5
Derek Peterson <derek.peterson@gpmtreit.com>
2025-09-03 14:15:29 -0500

    driver.R expansion

--- factor-decomp/R/driver.R
+++ factor-decomp/R/driver.R
@@ -1,10 +1,13 @@
 
 
-source("R/paths.R")         # defines data_raw_dir(), data_clean_dir()
-source("R/load_factors.R")  # defines load_ff_factors_daily()
-source("R/load_prices.R")   # defines download_prices()
-source("R/returns_utils.R") # defines prices_to_returns(), align_with_factors()
+# 1) Load source files for needed functions
+source("R/paths.R")          # defines data_raw_dir(), data_clean_dir()
+source("R/load_factors.R")   # defines load_ff_factors_daily()
+source("R/load_prices.R")    # defines download_prices()
+source("R/returns_utils.R")  # defines prices_to_returns(), align_with_factors()
+source("R/estimate_betas.R") # defines estimate_rolling_betas
 
+# 2) Define investment universe
 tickers <- c("SPY", # S&P 500, broad U.S. market
              "IWM", # Russell 2000 small caps
              "EFA", # Developed Markets ex-US
@@ -15,12 +18,26 @@ tickers <- c("SPY", # S&P 500, broad U.S. market
              "DBC", # Commodity index
              "VNQ") # REITs
 
+# 3) Download daily ticker prices, convert to daily percent returns 
+#    and combine with factors
 asset_prices <- download_prices(
   tickers      = tickers,
   start_date   = "2010-01-01",
   end_date     = Sys.Date()
 )
 
-asset_returns <- prices_to_returns()              # reads data_raw/ticker_prices.csv
+# convert prices to percent returns
+asset_returns <- prices_to_returns()            
+
+# load factors
 factors       <- load_ff_factors_daily()
+
+# join returns and factors
 aligned       <- align_with_factors(asset_returns, factors)
+
+
+# 4) Estimate rolling betas
+est <- estimate_rolling_betas(aligned, window = 252, min_obs = 180)
+
+dplyr::glimpse(est$betas)
+dplyr::glimpse(est$resid_var)
