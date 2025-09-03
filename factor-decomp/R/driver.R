201b6ec114b228d2783e6404af3455d32054c70a
Derek Peterson <derek.peterson@gpmtreit.com>
2025-09-03 15:20:56 -0500

    driver.R expansion

--- factor-decomp/R/driver.R
+++ factor-decomp/R/driver.R
@@ -6,6 +6,8 @@ source("R/load_factors.R")   # defines load_ff_factors_daily()
 source("R/load_prices.R")    # defines download_prices()
 source("R/returns_utils.R")  # defines prices_to_returns(), align_with_factors()
 source("R/estimate_betas.R") # defines estimate_rolling_betas
+source("R/covariances.R")
+source("R/risk_decomp.R")
 
 # 2) Define investment universe
 tickers <- c("SPY", # S&P 500, broad U.S. market
@@ -41,3 +43,35 @@ est <- estimate_rolling_betas(aligned, window = 252, min_obs = 180)
 
 dplyr::glimpse(est$betas)
 dplyr::glimpse(est$resid_var)
+
+
+# 5) Define weights
+# start with equal weight portfolio
+w <- c(SPY = 0.1111, IWM = 0.1111, EFA = 0.1111, 
+       EEM = 0.1111, LQD = 0.1111, HYG = 0.1111, 
+       TLT = 0.1111, DBC = 0.1111, VNQ = 0.1111)
+
+# Run the decomposition time series (uses same 252d / 180obs windows by default)
+betas_csv <- file.path(data_clean_dir(), "rolling_betas.csv")
+resid_csv <- file.path(data_clean_dir(), "rolling_resid_var.csv")
+
+decomp_ts <- portfolio_risk_timeseries(
+  betas_csv = betas_csv,
+  resid_csv = resid_csv,
+  factors_df = factors,
+  weights = w,
+  window = 252,
+  min_obs = 180
+)
+
+# Inspect latest date
+latest <- max(decomp_ts$date)
+decomp_latest <- dplyr::filter(decomp_ts, date == latest) %>%
+  arrange(share)
+decomp_latest
+
+annualized <- decomp_latest %>%
+  mutate(sd_annual = sd * sqrt(252)) %>%
+  mutate(pct_of_sd = sd / sd[factor == "TOTAL"])
+
+annualized
