4a43e1cfe37b6067eecd7374e436283eea888386
Derek Peterson <depeterson23@gmail.com>
2025-09-02 19:22:34 -0500

    driver.R update for secondary PC

--- factor-decomp/R/driver.R
+++ factor-decomp/R/driver.R
@@ -2,6 +2,7 @@
 
 source("R/paths.R")         # defines data_raw_dir(), data_clean_dir()
 source("R/load_factors.R")  # defines load_ff_factors_daily()
+source("R/load_prices.R")   # defines download_prices()
 source("R/returns_utils.R") # defines prices_to_returns(), align_with_factors()
 
 tickers <- c("SPY", # S&P 500, broad U.S. market
@@ -14,8 +15,12 @@ tickers <- c("SPY", # S&P 500, broad U.S. market
              "DBC", # Commodity index
              "VNQ") # REITs
 
-asset_prices <- download_prices(tickers)   
-asset_returns <- prices_to_returns()
+asset_prices <- download_prices(
+  tickers      = tickers,
+  start_date   = "2010-01-01",
+  end_date     = Sys.Date()
+)
 
-aligned    <- align_with_factors(asset_returns)  # factors auto-loaded
-dplyr::glimpse(aligned)
+asset_returns <- prices_to_returns()              # reads data_raw/ticker_prices.csv
+factors       <- load_ff_factors_daily()
+aligned       <- align_with_factors(asset_returns, factors)
